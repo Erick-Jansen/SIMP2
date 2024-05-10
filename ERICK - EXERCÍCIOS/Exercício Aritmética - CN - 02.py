092a982d7399fd53e70114d0c87220f20f7058f2
Erick-Jansen <erick050567@gmail.com>
2024-05-10 17:10:26 -0300

    Add files via upload

---
+++ ERICK - EXERCÍCIOS/Exercício Aritmética - CN - 02.py
@@ -0,0 +1,9 @@
+# Faça um algoritmo para ler três números e imprimir
+# a soma, mídia e produto dos números lidos.
+
+a = int(input("Escreva o primeiro número:"))
+b = int(input("Escreva o segundo número:"))
+c = int(input("Escreva o terceiro número:"))
+print(f'A soma desses números é igual a {a + b + c}')
+print(f'A média desses números é igual a {int((a + b + c)/3)}')
+print(f'O produto desses números é igual a {a * b * c}')
