5a298db8018c366e052146d558b23ac239718522
Erick-Jansen <erick050567@gmail.com>
2024-06-24 14:47:09 -0300

    Final

--- ERICK - EXERCÍCIOS/teste matrícula.py
+++ ERICK - EXERCÍCIOS/teste matrícula.py
@@ -1,73 +1,126 @@
 import os
 import datetime
 
-# Inicialização de uma lista para armazenar os dados dos alunos
+
 alunos = []
 contador_aluno = 0
 
-def gerar_matricula():
+def gerar_matricula():                                  
     global contador_aluno
     agora = datetime.datetime.now()
     ano = agora.year
     mes = agora.month
     contador_aluno += 1
-    numero_sequencial = f'{contador_aluno:04d}'  # Número sequencial com 4 dígitos
+    numero_sequencial = f'{contador_aluno:04d}'
     matricula = f'{ano}{mes:02d}{numero_sequencial}'
     return matricula
 
 def calcular_idade(data_nascimento):
     hoje = datetime.datetime.now()
     idade_anos = hoje.year - data_nascimento.year
-    idade_meses = hoje.month - data_nascimento.month
-
-    if idade_meses < 0:
+    if hoje.month < data_nascimento.month or (hoje.month == data_nascimento.month and hoje.day < data_nascimento.day):
         idade_anos -= 1
-        idade_meses += 12
+    return idade_anos
 
-    return idade_anos, idade_meses
+def validar_data_nascimento(data_str):
+    try:
+        if len(data_str) != 8 or not data_str.isdigit():
+            raise ValueError("Formato inválido")
+        dia = int(data_str[:2])
+        mes = int(data_str[2:4])
+        ano = int(data_str[4:])
+        data_nascimento = datetime.datetime(ano, mes, dia)
+        idade_anos = calcular_idade(data_nascimento)
+        if idade_anos < 4:
+            raise ValueError("Aluno deve ter pelo menos 4 anos de idade")
+        return data_nascimento
+    except ValueError as e:
+        print(f"{e}. Tente novamente.")
+        return None
 
 def cadastrar_aluno():
+    os.system('cls' if os.name == 'nt' else 'clear')
+    
     while True:
-        nome = input('Insira o nome do aluno (nome e sobrenome): ')
-        if ' ' in nome.strip() and all(part.isalpha() for part in nome.split()):
+        nome = input('Insira o nome do aluno (nome e sobrenome): ').strip().title()
+        if ' ' in nome and all(part.isalpha() for part in nome.split()):
             break
         else:
             print("Por favor, insira um nome válido contendo apenas letras, com pelo menos um nome e um sobrenome.")
     
     while True:
-        try:
-            dia = int(input('Insira o dia de nascimento (DD): '))
-            mes = int(input('Insira o mês de nascimento (MM): '))
-            ano = int(input('Insira o ano de nascimento (AAAA): '))
-            data_nascimento = datetime.datetime(ano, mes, dia)
-            idade_anos, idade_meses = calcular_idade(data_nascimento)
+        data_nascimento_str = input('Insira a data de nascimento do aluno (ddmmaaaa): ').strip()
+        data_nascimento = validar_data_nascimento(data_nascimento_str)
+        if data_nascimento:
+            idade_anos = calcular_idade(data_nascimento)
             break
-        except ValueError:
-            print("Data de nascimento inválida. Tente novamente.")
 
-    email = input('Insira o e-mail do aluno: ')
-    endereco = input('Insira o endereço do aluno: ')
-    contato_aluno = input('Insira o número de contato do aluno: ')
-    nome_responsavel = input('Escreva o nome do responsável: ')
-    contato_responsavel = input('Escreva o número para contato do responsável: ')
-    identidade = input('Escreva o RG do aluno: ')
-    grau_escolaridade = input('Qual o grau que o aluno está sendo matrículado?: ')
+    while True:
+        email = input('Insira o e-mail do aluno: ').strip()
+        if "@" in email and email.endswith(".com"):
+            break
+        else:
+            print("Por favor, insira um e-mail válido contendo '@' e terminando com '.com'.")
+
+    rua = input('Insira a rua do aluno: ').strip().title()
+    bairro = input('Insira o bairro do aluno: ').strip().title()
+    
+    while True:
+        numero = input('Insira o número da residência do aluno (até 6 dígitos): ').strip()
+        if numero.isdigit() and len(numero) <= 6:
+            break
+        else:
+            print("Por favor, insira um número válido para a residência com até 6 dígitos.")
+
+    endereco = f'Rua: {rua}, Bairro: {bairro}, Número: {numero}'
+
+    while True:
+        contato_aluno = input('Insira o número de contato do aluno (11 dígitos): ').strip()
+        if contato_aluno.isdigit() and len(contato_aluno) == 11:
+            contato_aluno_formatado = f'({contato_aluno[:2]}) {contato_aluno[2]} {contato_aluno[3:7]}-{contato_aluno[7:]}'
+            break
+        else:
+            print("Por favor, insira um número de contato válido com 11 dígitos.")
+
+    while True:
+        nome_responsavel = input('Escreva o nome do responsável: ').strip().title()
+        if ' ' in nome_responsavel and all(part.isalpha() for part in nome_responsavel.split()):
+            break
+        else:
+            print("Por favor, insira um nome válido contendo apenas letras, com pelo menos um nome e um sobrenome.")
+
+    while True:
+        contato_responsavel = input('Escreva o número para contato do responsável (11 dígitos): ').strip()
+        if contato_responsavel.isdigit() and len(contato_responsavel) == 11:
+            contato_responsavel_formatado = f'({contato_responsavel[:2]}) {contato_responsavel[2]} {contato_responsavel[3:7]}-{contato_responsavel[7:]}'
+            break
+        else:
+            print("Por favor, insira um número de contato válido com 11 dígitos.")
+
+    while True:
+        identidade = input('Escreva o RG do aluno (7 a 11 dígitos): ').strip()
+        if identidade.isdigit() and 7 <= len(identidade) <= 11:
+            identidade_formatada = f'{identidade[:-1]}-{identidade[-1]}'
+            break
+        else:
+            print("Por favor, insira um RG válido com 7 a 11 dígitos.")
+    
+    grau_escolaridade = input('Qual o grau que o aluno está sendo matrículado?: ').strip().title()
     
     matricula = gerar_matricula()
     
     aluno = {
         'nome': nome,
-        'data_nascimento': data_nascimento,
+        'data_nascimento': data_nascimento.strftime("%d/%m/%Y"),
         'idade_anos': idade_anos,
-        'idade_meses': idade_meses,
         'email': email,
         'endereco': endereco,
-        'contato_aluno': contato_aluno,
+        'contato_aluno': contato_aluno_formatado,
         'nome_responsavel': nome_responsavel,
-        'contato_responsavel': contato_responsavel,
+        'contato_responsavel': contato_responsavel_formatado,
         'grau_escolaridade': grau_escolaridade,
         'matricula': matricula,
-        'identidade': identidade,
+        'identidade': identidade_formatada,
     }
     
     alunos.append(aluno)
@@ -76,15 +129,47 @@ def cadastrar_aluno():
     os.system('cls' if os.name == 'nt' else 'clear')
 
 def consultar_aluno():
-    os.system('cls' if os.name == 'nt' else 'clear')
-    if not alunos:
-        print("Nenhum aluno cadastrado.\n")
-    else:
-        for aluno in alunos:
-            print(f"Nome: {aluno['nome']}, Idade: {aluno['idade_anos']} anos e {aluno['idade_meses']} meses, E-mail: {aluno['email']}, Contato: {aluno['contato_aluno']}, Matrícula: {aluno['matricula']}")
-        print("")
-    input("Pressione Enter para continuar...")
-    os.system('cls' if os.name == 'nt' else 'clear')
+    while True:
+        os.system('cls' if os.name == 'nt' else 'clear')
+        if not alunos:
+            print("Nenhum aluno cadastrado.\n")
+        else:
+            print("a. Consultar aluno por matrícula")
+            print("b. Consultar dados dos responsáveis")
+            print("c. Listar todos os alunos matriculados")
+            print("d. Voltar ao menu principal")
+            opcao = input("Digite sua opção (a, b, c ou d): ")
+
+            if opcao == "a":
+                matricula = input("Digite a matrícula do aluno: ").strip()
+                encontrado = False
+                for aluno in alunos:
+                    if aluno['matricula'] == matricula:
+                        encontrado = True
+                        print(f"\nDados do Aluno:\nNome: {aluno['nome']}\nData de Nascimento: {aluno['data_nascimento']}\nIdade: {aluno['idade_anos']} anos\nE-mail: {aluno['email']}\nEndereço: {aluno['endereco']}\nContato: {aluno['contato_aluno']}\nNome do Responsável: {aluno['nome_responsavel']}\nContato do Responsável: {aluno['contato_responsavel']}\nGrau Escolaridade: {aluno['grau_escolaridade']}\nMatrícula: {aluno['matricula']}\nIdentidade: {aluno['identidade']}")
+                        break
+                if not encontrado:
+                    print(f"Aluno com matrícula {matricula} não encontrado.")
+            elif opcao == "b":
+                matricula = input("Digite o nome completo do aluno para consultar os dados dos responsáveis: ").strip()
+                encontrado = False
+                for aluno in alunos:
+                    if aluno['matricula'] == matricula:
+                        encontrado = True
+                        print(f"\nDados do Responsável:\nNome: {aluno['nome_responsavel']}\nContato: {aluno['contato_responsavel']}")
+                        break
+                if not encontrado:
+                    print(f"Aluno de matrícula {matricula} não encontrado.")
+            elif opcao == "c":
+                print("\nLista de todos os alunos matriculados:")
+                for aluno in alunos:
+                    print(f"Nome: {aluno['nome']}, Matrícula: {aluno['matricula']}")
+            elif opcao == "d":
+                break
+            else:
+                print("Opção inválida! Por favor, escolha a, b, c ou d.")
+        
+        input("\nPressione Enter para continuar...")
 
 def excluir_aluno():
     os.system('cls' if os.name == 'nt' else 'clear')
